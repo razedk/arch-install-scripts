26519dc68c0af3b07c7f66c65c45111b021cf032
Jacob Lundholm <jal@intelligentsystems.dk>
2026-03-16 11:43:51 -0500

    Updated to use stow and a dotfiles folder

--- install-shell-addons.sh
+++ install-shell-addons.sh
@@ -3,7 +3,7 @@
 #
 # This scripts downloads and installs starship cross-shell prompt
 #
-# NOTE: Script is made for Fedora and might not work on other distributions
+# NOTE: Script is made for Arch and might not work on other distributions
 #
 
 source config/common-config.sh
@@ -15,7 +15,7 @@ source util/file-util.sh
 # F U N C T I O N S   S E C T I O N
 #
 
-INSTALL_PATH="$(pwd)/install-bash-addons"
+INSTALL_PATH="$(pwd)/dotfiles"
 
 command_exists () {
     command -v $1 >/dev/null 2>&1;
@@ -24,25 +24,13 @@ command_exists () {
 installDepend() {
     # Check for dependencies.
     print_info "Installing dependencies..."
-    sudo pacman -S zoxide fzf bash-completion tar bat starship ttf-firacode-nerd stow eza fd
-}
-
-installStarship() {
-    if command_exists starship; then
-        print_ok "Starship already installed"
-        return
-    fi
-
-    if ! curl -sS https://starship.rs/install.sh|sh;then
-        print_error "Something went wrong during starship install!"
-        exit 1
-    fi
+    sudo pacman -S zoxide fzf bash-completion tar bat starship ttf-firacode-nerd stow eza fd fish
 }
 
 installConfig() {
-    print_info "Sym linking config files (${INSTALL_PATH})..."
-    rm ${HOME}/.bashrc 
-    stow -t ${HOME} -d ${INSTALL_PATH} .
+    print_info "Sym linking config files in (${INSTALL_PATH})..."
+    rm -rf ${HOME}/.config/nvim ${HOME}/.config/rvim ${HOME}/.config/starship.toml ${HOME}/.config/wezterm ${HOME}/.config/fish ${HOME}/.bashrc ${HOME}/.bashrc.d 
+    stow -d ${INSTALL_PATH} -t ${HOME} nvim rvim wezterm bash fish starship  
 }
 
 configNerdFont() {
@@ -56,7 +44,6 @@ configNerdFont() {
 #
 
 installDepend
-installStarship
 installConfig
 configNerdFont
 
